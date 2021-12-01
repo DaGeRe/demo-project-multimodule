cbdff35ce7ca853e205a83d9168ce20bce38f9ba
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-12-01 11:39:12 +0100

    Introduce regression

--- using/src/main/java/de/dagere/using/AnotherChangeable.java
+++ using/src/main/java/de/dagere/using/AnotherChangeable.java
@@ -3,7 +3,12 @@ package de.dagere.using;
 public class AnotherChangeable {
 
 	public void callMe() {
-
+try {
+         Thread.sleep(50);
+      } catch (InterruptedException e1) {
+         // TODO Auto-generated catch block
+         e1.printStackTrace();
+      }
 	}
 
 }
